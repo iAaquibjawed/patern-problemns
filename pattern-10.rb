3be813681d26f05217fde359a4c2a270133fcd78
= <=>
2024-05-10 11:07:53 +0530

    pattern-10 problem

---
+++ pattern-10.rb
@@ -0,0 +1,18 @@
+for i in 0..5
+    for k in 0..i
+        print " "
+    end
+     for j in 0..5-i
+        print "*"
+    end
+    puts ""
+end
+
+
+
+# ******
+#  *****
+#   ****
+#    ***
+#     **
+#      *
