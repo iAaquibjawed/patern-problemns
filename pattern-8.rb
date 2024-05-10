cf47618247f6c62821aea8f66abb755211626e11
= <=>
2024-05-10 10:41:18 +0530

    pattern 8 problem

---
+++ pattern-8.rb
@@ -0,0 +1,19 @@
+for i in 1..5
+    for j in 1..5-i 
+        print " "
+    end
+    for k in 1..i
+        print "*"
+    end
+    for l in 1..i-1
+        print "*"
+    end
+    puts ""
+end
+
+
+#     *
+#    ***
+#   *****
+#  *******
+# *********
