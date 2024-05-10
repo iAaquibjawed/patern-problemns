a51c24ca2400dac5eac693ab4e29dbfd0e2035cb
= <=>
2024-05-10 11:09:45 +0530

    pattern-11 problem

---
+++ pattern-11.rb
@@ -0,0 +1,20 @@
+for i in 0..5
+    for k in 0..i 
+        print " "
+    end
+    for j in 0..5-i
+        print "*"
+    end
+    for l in 0..5-i-1
+        print "*"
+    end
+    puts ""
+end
+
+
+# ***********
+#  *********
+#   *******
+#    *****
+#     ***
+#      *
