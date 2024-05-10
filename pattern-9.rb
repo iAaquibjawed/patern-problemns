67d58c4ebe060dbb5b3f9c6aae3daf354fc58dea
= <=>
2024-05-10 10:56:03 +0530

    pattern-9 problem

---
+++ pattern-9.rb
@@ -0,0 +1,18 @@
+for i in 0..5 
+    for j in 1..5-i
+        print "*"
+    end
+    for k in 0..i
+        print " "
+    end
+    puts ""
+end
+
+
+
+
+# ***** 
+# ****  
+# ***   
+# **    
+# *  
